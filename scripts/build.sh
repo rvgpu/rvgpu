a6ec055993e7ef8c58a57463cc0d5c09116d9121
Qi Hangkong <qihangkong@outlook.com>
2023-04-25 17:41:49 +0800

    build: build rvgpu-cmodel generate librvgsim.so and libdrm.so

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,19 +10,37 @@ function build_env
 function build_mesa
 {
     mesa_dir=${PWD}/rvgpu-mesa
-    build_dir=${PWD}/build/rvgpu-mesa
-    install_dir=${PWD}/install
+    mesa_build_dir=${PWD}/build/rvgpu-mesa
+    mesa_install_dir=${PWD}/install
 
     if [ -f rvgpu-mesa/README.md ]; then
-        if [ ! -d ${build_dir} ]; then
-            meson ${build_dir} rvgpu-mesa -Dprefix=${install_dir} -Dgallium-drivers= -Dvulkan-drivers=rvgpu -Dplatforms=x11 -Dglx=disabled -Dbuildtype=debug
+        if [ ! -d ${mesa_build_dir} ]; then
+            meson ${mesa_build_dir} rvgpu-mesa -Dprefix=${mesa_install_dir} -Dgallium-drivers= -Dvulkan-drivers=rvgpu -Dplatforms=x11 -Dglx=disabled -Dbuildtype=debug
         fi
-        ninja -C ${build_dir} install
+        ninja -C ${mesa_build_dir} install
     else
         echo "rvgpu-mesa is a illegal repos under this project"
+    fi
+}
+
+function build_rvgsim
+{
+    rvgsim_dir=${PWD}/rvgpu-cmodel
+    rvgsim_build_dir=${PWD}/build/rvgpu-cmodel
+    rvgsim_install_dir=${PWD}/install
+    if [ -f rvgpu-cmodel/README.md ]; then
+        if [ ! -d ${rvgsim_build_dir} ]; then
+            mkdir -p ${rvgsim_build_dir}
+            cmake -B ${rvgsim_build_dir} ${rvgsim_dir} -DCMAKE_INSTALL_PREFIX=${rvgsim_install_dir}
+        fi
+        cmake --build ${rvgsim_build_dir}
+        cmake --install ${rvgsim_build_dir}
+    else
+        echo "rvgpu-cmodel is a illegal repos under this project"
     fi
 }
 
 build_env
 
 build_mesa
+build_rvgsim
